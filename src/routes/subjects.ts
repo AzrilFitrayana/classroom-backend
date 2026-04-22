e43fbb40001bfdadfea7366c0477ecbf1b2b99fb
azril fitrayana <azrilfitrayana77@gmail.com>
2026-04-22 07:29:09 +0700

    implements GET all subjects route

---
+++ src/routes/subjects.ts
@@ -0,0 +1,77 @@
+import { and, desc, eq, getTableColumns, ilike, or, sql } from "drizzle-orm";
+import express from "express";
+import { departments, subjects } from "../db/schema";
+import { db } from "../db";
+
+const router = express.Router();
+
+// Get all subjects with optional serach, filtering and pagination
+router.get("/", async (req, res) => {
+  try {
+    const { search, department, page = 1, limit = 10 } = req.query;
+
+    const currentPage = Math.max(1, +page);
+    const limitPerPage = Math.max(1, +limit);
+
+    const offset = (currentPage - 1) * limitPerPage;
+
+    const filterConditions = [];
+
+    // If search query exist, filter by subject name OR subject code
+    if (search) {
+      filterConditions.push(
+        or(
+          ilike(subjects.name, `%${search}%`),
+          ilike(subjects.code, `%${search}%`),
+        ),
+      );
+    }
+
+    // If department query exist, filter by department
+    if (department) {
+      filterConditions.push(ilike(departments.name, `%${department}%`));
+    }
+
+    // Combine all filter using AND if any exist
+    const whereClause =
+      filterConditions.length > 0 ? and(...filterConditions) : undefined;
+
+    // Get total count of subjects matching the filter conditions for pagination
+    const countResult = await db
+      .select({ count: sql<number>`count(*)` })
+      .from(subjects)
+      .leftJoin(departments, eq(subjects.departmentId, departments.id))
+      .where(whereClause);
+
+    // Extract total count from the result
+    const totalCount = countResult[0]?.count || 0;
+
+    // Fetch the subjects with the applied filters, pagination and sorting
+    const subjectsList = await db
+      .select({
+        ...getTableColumns(subjects),
+        department: { ...getTableColumns(departments) },
+      })
+      .from(subjects)
+      .leftJoin(departments, eq(subjects.departmentId, departments.id))
+      .where(whereClause)
+      .orderBy(desc(subjects.createdAt))
+      .limit(limitPerPage)
+      .offset(offset);
+
+    res.status(200).json({
+      data: subjectsList,
+      pagination: {
+        page: currentPage,
+        limit: limitPerPage,
+        total: totalCount,
+        totalPages: Math.ceil(totalCount / limitPerPage),
+      },
+    });
+  } catch (error) {
+    console.error("GET /subjects error:", error);
+    res.status(500).json({ message: "Failed to fetch subjects" });
+  }
+});
+
+export default router;
